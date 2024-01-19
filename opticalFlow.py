7b0b3d40befd316e62f60e2d3ddcc9ac56a8bdf6
kayoooooo <kevinyao2016@gmail.com>
2024-01-19 16:42:16 -0500

    attempting drawing a box, optimization

--- opticalFlow.py
+++ opticalFlow.py
@@ -21,6 +21,52 @@ def draw_hsv(flow):
     
     return bgr
 
+def draw_box(flow):
+    fx,fy = flow[:,:,0], flow[:,:,1] 
+    
+    v = np.sqrt(fx*fx + fy*fy)
+    
+    topLeftx = 0
+    topLefty = 0
+    y = 0
+    x = 0
+    out = false
+    while y < len(v):
+        while x < len(v[0]):
+            if v[y,x] > 50:
+                topLeftx = x
+                topLefty = y
+                out = true
+                break
+            x += 10
+        if out:
+            break
+        x = 0
+        y += 10
+    
+    botRightx = 0
+    botRighty = 0
+    y = len(v)-1
+    x = len(v[0])-1
+    out = false
+    while y > 0:
+        while x > 0:
+            if v[y, x] > 50:
+                botRightx = x
+                botRighty = y
+                out = true
+                break
+            x -= 10
+        if out:
+            break
+        x = len(v[0])-1
+        y -= 10
+    
+    bgr = np.zeros((h,w,3), np.uint8)
+    cv2.rectangle(bgr, (topLeftx,topLefty), (botRightx,botRighty), (0,255,0))
+    
+    return bgr
+
 capture = cv2.VideoCapture(0) #should be webcam
 
 
@@ -33,16 +79,19 @@ while True:
     
     start = time.time()
     
-    flow = cv2.calcOpticalFlowFarneback(gray_prev, gray_frame, None, .5, 3, 15, 3, 5, 1.2, 0)
+    flow = cv2.calcOpticalFlowFarneback(gray_prev, gray_frame, None, .5, 3, 15, 2, 5, 1.2, 0)
+    #flow = cv2.calcOpticalFlowPyrLK(gray_prev, gray_frame) may attempt sparce optical flow
     
     gray_prev = gray_frame #preparation for next frame/loop
+    
+    img = draw_hsv(flow) 
+    #box = draw_box(flow) 
     
     end = time.time()
     deltaT = end - start
     fps = 1/deltaT
     print(fps)
     
-    img = draw_hsv(flow) 
     cv2.putText(img, 
     text = "FPS: " + str(fps), 
     org = (10,30), 
@@ -54,6 +103,7 @@ while True:
     #this is for viewing results, would most likely be removed.
     #cv2.imshow("Image", gray_frame)
     cv2.imshow("HSV", img)
+    #cv2.imshow("BOX", box)
     key = cv2.waitKey(1)
     if key == 27:
         break
